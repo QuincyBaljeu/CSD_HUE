46c4af6c3ea478637fb903293f3d4520948adf6a
Quincy Baljeu <q.baljeu@gmail,com>
2019-11-20 15:24:51 +0100

    added hue controller activity

---
+++ app/src/main/java/com/example/csd_hue/HueController.java
@@ -0,0 +1,14 @@
+package com.example.csd_hue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HueController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hue_controller);
+    }
+}
