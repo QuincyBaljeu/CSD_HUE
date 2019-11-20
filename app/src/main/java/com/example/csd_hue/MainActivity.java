112547173b9967887a86f389f9444ff3567666b9
Quincy Baljeu <q.baljeu@gmail,com>
2019-11-20 16:18:17 +0100

    added translations to text

--- app/src/main/java/com/example/csd_hue/MainActivity.java
+++ app/src/main/java/com/example/csd_hue/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.csd_hue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button_Connect = findViewById(R.id.btn_connect);
+
+        button_Connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HueController.class));
+            }
+        });
     }
 }
